4b02e53e5febd93f37974ea0f9d72ecd960a7a53
Futar0 <104120854+Futar0@users.noreply.github.com>
2023-02-04 10:17:37 +0800

    Update bootleg_lancelot.mk

--- bootleg_lancelot.mk
+++ bootleg_lancelot.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/lancelot/device.mk)
 
 # Inherit some common Spark OS stuff.
-$(call inherit-product, vendor/bootleg/config/common_full_phone.mk)
+$(call inherit-product, vendor/bootleg/config/common.mk)
 
 PRODUCT_NAME := bootleg_lancelot
 PRODUCT_DEVICE := lancelot
